64c7298a7c8da347230d546539cd4d98ecd645ad
Marembo Ochieng <marembo2008@gmail.com>
2015-01-11 11:55:26 +0100

    table name

--- src/main/java/com/anosym/elexar/facade/ElectiveRegionBoundaryFacade.java
+++ src/main/java/com/anosym/elexar/facade/ElectiveRegionBoundaryFacade.java
@@ -6,6 +6,7 @@ import com.anosym.elexar.controller.ElexarController;
 import com.anosym.jflemax.geometry.Coordinate;
 import com.anosym.jflemax.geometry.Polygon;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import javax.ejb.EJB;
 import javax.ejb.Stateless;
@@ -36,15 +37,14 @@ public class ElectiveRegionBoundaryFacade extends AbstractFacade<ElectiveRegionB
     /**
      * Loads the elective region with the coordinates data decoded.
      *
-     * @param regionId
-     * @param electiveRegionType
+     * @param electiveRegion
      * @return
      */
     public ElectiveRegionBoundary findElectiveRegionBoundary(ElectiveRegion electiveRegion) {
         //we use native sql
         String sql = "SELECT "
-                + "BOUNDARYID, AsText(CENTRECOORDINATE), AsText(REGIONBOUNDARIES) "
-                + "FROM ELECTIVEREGIONBOUNDARY WHERE ELECTIVEREGION_REGIONID = " + electiveRegion.getRegionId();
+                + "boundaryId, AsText(centreCoordinate), AsText(regionBoundaries) "
+                + "FROM ElectiveRegionBoundary WHERE electiveRegion_regionId = " + electiveRegion.getRegionId();
         try {
             Object[] data = (Object[]) getEntityManager().createNativeQuery(sql).getSingleResult();
             if (data != null) {
@@ -60,14 +60,14 @@ public class ElectiveRegionBoundaryFacade extends AbstractFacade<ElectiveRegionB
         try {
             String tableName = electiveRegion.getElectiveRegionType().getChildPersistentEntityName();
             String sql = "SELECT "
-                    + "BOUNDARYID, AsText(CENTRECOORDINATE),AsText(REGIONBOUNDARIES), ELECTIVEREGION_REGIONID "
-                    + "FROM ELECTIVEREGIONBOUNDARY AS B JOIN " + tableName + " AS E ON B.ELECTIVEREGION_REGIONID = E.REGIONID WHERE E.PARENTELECTIVEREGION_REGIONID = " + electiveRegion
+                    + "boundaryId, AsText(centreCoordinate),AsText(regionBoundaries), electiveRegion_regionId "
+                    + "FROM ElectiveRegionBoundary AS B JOIN " + tableName + " AS E ON B.electiveRegion_regionId = E.regionId WHERE E.parentElectiveRegion_regionId = " + electiveRegion
                     .getRegionId();
             List<Object[]> list = getEntityManager()
                     .createNativeQuery(sql)
                     .getResultList();
             if (list != null && !list.isEmpty()) {
-                List<ElectiveRegionBoundary> electiveRegionBoundaries = new ArrayList<ElectiveRegionBoundary>();
+                List<ElectiveRegionBoundary> electiveRegionBoundaries = new ArrayList<>();
                 for (Object[] data : list) {
                     Object id = data[3];
                     ElectiveRegion region = electiveRegionFacade.find(id);
@@ -79,7 +79,7 @@ public class ElectiveRegionBoundaryFacade extends AbstractFacade<ElectiveRegionB
         } catch (Exception e) {
             ElexarController.logError(e);
         }
-        return new ArrayList<ElectiveRegionBoundary>();
+        return Collections.EMPTY_LIST;
     }
 
     private ElectiveRegionBoundary decodeElectiveRegionBoundary(Object[] data, ElectiveRegion electiveRegion) {
